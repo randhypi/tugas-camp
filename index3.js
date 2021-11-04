5041bafb0ab11283384651b1995ab9cf8bb8a048
Randhy <randhypradanairsan@gmail.com>
2021-11-04 09:16:51 +0700

    done

--- index3.js
+++ index3.js
@@ -1,3 +1,29 @@
 var filterBooksPromise = require('./promise2.js')
 
 // Lanjutkan code untuk menjalankan function filterBookPromise
+
+ async function response (isColor,amount){
+  let result = await filterBooksPromise
+  return result(isColor,amount)
+
+}
+
+response(true,40).then(
+value => {
+    console.log(value)
+}
+)
+
+
+response(false,250).then(
+    value => {
+        console.log(value)
+    }
+    )
+
+    
+response(true,30).catch(e=>{
+        console.log(e.message)
+         }
+        
+        )
